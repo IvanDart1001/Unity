13d118268224d553bfe217288d72a4e2d6714eb8
Yisaer <2695690803@qq.com>
2016-12-03 16:49:47 +0800

    MiniGame End

--- Yisa/MiniGame/Assets/Script/PlayerController2.cs
+++ Yisa/MiniGame/Assets/Script/PlayerController2.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
+
 
 public class PlayerController2 : MonoBehaviour {
 
 
 	public float speed;
+	public Text CountText;
 
 
 	private Rigidbody rb;
+	private int Count ;
+	public Text winText;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
-
+		Count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	/*
@@ -25,6 +33,25 @@ public class PlayerController2 : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter (Collider ohter){
+		if( ohter.gameObject.CompareTag("PickUp")){
+			
+			ohter.gameObject.SetActive (false);
+
+			Count = Count + 1;
+			SetCountText ();
+
+		}
+	}
+
+	void SetCountText(){
+		CountText.text = "Count: " + Count.ToString ();
+		if(Count>=10){
+			winText.text = "You Win";
+		}
 
 	}
 
